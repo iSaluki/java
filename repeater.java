64b453d72a64a66a34bab2689a410c7028741a4e
Seth <37915796+iSaluki@users.noreply.github.com>
2021-02-04 18:23:04 +0000

    Create repeater.java

---
+++ repeater.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Repeater
+{
+  public static void main (String[] args) 
+  {
+    String inData;
+    Scanner scan = new Scanner( System.in );
+
+    System.out.println("Enter message: ");
+    message = scan.nextLine();
+
+    System.out.println("You typed: " + message );
+  }
+}
